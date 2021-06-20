1d2964564bf7764cd144c5736bf331fc9458ced6
Syahdi Akbar <syahdiakbar99@gmail.com>
2021-06-20 20:36:55 +0700

    Update konter.sql

--- db/konter.sql
+++ db/konter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Bulan Mei 2021 pada 09.48
+-- Waktu pembuatan: 20 Jun 2021 pada 15.34
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.3.15
 
@@ -50,6 +50,30 @@ CREATE TABLE `beli` (
   `total` double(12,2) NOT NULL,
   `status` enum('dipesan','dibayar','diterima','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_banner`
+--
+
+CREATE TABLE `data_banner` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `descript` varchar(255) NOT NULL,
+  `banner_date` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `urutan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_banner`
+--
+
+INSERT INTO `data_banner` (`id`, `name`, `image`, `descript`, `banner_date`, `urutan`) VALUES
+(1, 'Slide-01', 'slide-001.jpg', 'This is for you', '2021-06-20 13:30:42', 1),
+(2, 'Slide-02', 'slide-002.jpg', 'Yeah you can see this', '2021-06-20 13:28:19', 2),
+(3, 'Slide-03', 'slide-003.jpg', 'Now you can see me', '2021-06-20 13:29:04', 3);
 
 -- --------------------------------------------------------
 
@@ -146,7 +170,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `no_telp`, `alamat`, `role_id`, `is_active`, `date_created`, `no_rekening`, `nama_bank`) VALUES
-(1, 'Admin', 'admin@gmail.com', 'default.png', '$2y$10$66ouz6vCWvwnuJBtx7B.uevW9EBjKrYq0pHSITDOD678qjudzmPE2', '6281336787990', 'Jl Admin 221', 1, 1, 1621959515, '-', '-'),
+(1, 'Admin', 'admin@gmail.com', 'avatar.png', '$2y$10$OCvmvrtPHbClwbGjqQY.4u1s2jGquK80tMMqd.nWlDNqBaxohnJXy', '6281336787990', 'Jl Admin 221', 1, 1, 1621959515, '-', '-'),
 (2, 'Penjual', 'penjual@gmail.com', 'default.png', '$2y$10$osgIK9gqBofOfu/tgQB9bOkI5wrFit2lutKDGzty2D2lBOZTYNt8a', '6287880900655', 'Jl Penjual 887', 2, 1, 1621959595, '0007000100020003', 'BRI'),
 (3, 'Pembeli', 'pembeli@gmail.com', 'default.png', '$2y$10$EtzFzgWxjmZb/WkpR2ovaOkcU6O.y1VhG.BHP/yrrDG917/ipFNYG', '6285000988123', 'Jl Pembeli 223', 3, 1, 1621959665, '-', '-');
 
@@ -233,14 +257,16 @@ CREATE TABLE `user_sub_menu` (
 --
 
 INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `urutan`, `is_active`) VALUES
-(1, 1, 'Dashboard Admin', 'admin', 'fas fa-fw fa-tachometer-alt', 1, 1),
-(2, 2, 'Dashboard Penjual', 'penjual', 'fas fa-fw fa-tachometer-alt', 1, 1),
-(3, 1, 'My Profile', 'admin/my_profile', 'far fa-fw fa-user', 8, 1),
-(4, 2, 'My Profile', 'penjual/my_profile', 'far fa-fw fa-user', 8, 1),
-(5, 1, 'Edit Profile', 'admin/edit_profile', 'fa fa-fw fa-user-edit', 9, 1),
-(6, 2, 'Edit Profile', 'penjual/edit_profile', 'fa fa-fw fa-user-edit', 9, 1),
-(7, 1, 'Change Password', 'admin/change_password', 'fa fa-fw fa-key', 10, 1),
-(8, 2, 'Change Password', 'penjual/change_password', 'fa fa-fw fa-key', 10, 1);
+(1, 1, 'Dashboard Admin', 'Admin', 'fas fa-fw fa-tachometer-alt', 1, 1),
+(2, 2, 'Dashboard Penjual', 'Penjual', 'fas fa-fw fa-tachometer-alt', 1, 1),
+(3, 1, 'My Profile', 'Admin/my_profile', 'far fa-fw fa-user', 8, 1),
+(4, 2, 'My Profile', 'Penjual/my_profile', 'far fa-fw fa-user', 8, 1),
+(5, 1, 'Edit Profile', 'Admin/edit_profile', 'fa fa-fw fa-user-edit', 9, 1),
+(6, 2, 'Edit Profile', 'Penjual/edit_profile', 'fa fa-fw fa-user-edit', 9, 1),
+(7, 1, 'Change Password', 'Admin/change_password', 'fa fa-fw fa-key', 10, 1),
+(8, 2, 'Change Password', 'Penjual/change_password', 'fa fa-fw fa-key', 10, 1),
+(9, 1, 'Kelola User', 'Admin/kelola_user', 'fas fa-fw fa-users', 2, 1),
+(10, 1, 'Kelola Banner', 'Admin/kelola_banner', 'fas fa-fw fa-folder', 3, 1);
 
 --
 -- Indexes for dumped tables
@@ -257,6 +283,12 @@ ALTER TABLE `barang`
 --
 ALTER TABLE `beli`
   ADD PRIMARY KEY (`id_beli`);
+
+--
+-- Indeks untuk tabel `data_banner`
+--
+ALTER TABLE `data_banner`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `jenis`
@@ -311,6 +343,12 @@ ALTER TABLE `user_sub_menu`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `data_banner`
+--
+ALTER TABLE `data_banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -338,7 +376,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT untuk tabel `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
